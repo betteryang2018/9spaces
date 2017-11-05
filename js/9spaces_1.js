c31c74470cbd1cf79d3fd332801784b1f93d2d1c
cocoyang2017 <190024985@qq.com>
2017-11-05 11:07:12 +0800

    modify

--- js/9spaces_1.js
+++ js/9spaces_1.js
@@ -9,9 +9,7 @@ function byId(id){
 var start=byId("start"),
 	end=byId("end"),
 	tds=byId("tb_wrap").getElementsByTagName("td"),
-	colors=['red','plum','blue','green','cyan','black','pink','gray','brown'],
-	arr=new Array(3),  // 创建数组容纳随机数 
-	arr1=new Array(3),                 
+	arr=new Array(3),  // 创建数组容纳随机数
 	timer=null;
 	
 // 点击开始	
@@ -19,6 +17,9 @@ start.onclick=function(){
 	timer=setInterval(function(){
 		startFun();
 	},1000);
+	start.onclick=function(){
+		return;
+	}
 }
 
 // 停止按钮
@@ -38,21 +39,18 @@ function clear(){
 function startFun(){
 	// 每次随机颜色时遍历将背景设置好
 	clear();
-	// 获取随机地址、随机颜色的数组
+	// 获取随机地址的数组
 	for(var i=0;i<3;i++){  
 		var a=parseInt(Math.random()*9);
-		var b=parseInt(Math.random()*9);
 		if(i==0){  
 			arr[i]=a;
-			arr1[i]=b;
 		}else{     
 			for(var j=0;j<i;j++){ 
 			    // 如果重复从新开始
-				if(a==arr[j]||b==arr1[j]){ 
+				if(a==arr[j]){ 
 					i--;
 				}else{
 					arr[i]=a;
-					arr1[i]=b;
 				}
 			}
 		}
@@ -60,11 +58,27 @@ function startFun(){
 	
 	// 将随机的颜色给随机的地址
 	for(var i=0;i<arr.length;i++){
-		tds[arr[i]].style.background=colors[arr1[i]];
+		// 获取随机颜色
+    	var colorsStr=colorsString();
+		tds[arr[i]].style.background="#"+colorsStr;
 	}
 }
 }
 
+// 生成随机颜色colorsString();
+function colorsString(){
+	var letNum=['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9],
+		colors=[];  // 用来装随机颜色
+	for(var i=0;i<6;i++){
+		var n=Math.floor(Math.random()*16);
+		colors.push(letNum[n]);
+	}
+		var colorsStr=colors.join("");
+		if(colorsStr=="ffa600"){
+			colorsString();
+		}
+		return colorsStr;
+}
 
 
 
